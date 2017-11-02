3d0182f2f8c0329bcff309f576de27d57c620207
mdcollins80 <mdcollins80@gmail.com>
2017-11-02 15:32:56 -0400

    Adds updateReview action on review route
    - Adds updateReview action to review-update component.
    - Action passes up to the review.js route.
    - Data updates and persists to the database.

--- app/components/review-update.js
+++ app/components/review-update.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  text: null,
+  actions: {
+    updateReview () {
+      const review = {
+        id: this.get('review.id'),
+        qowp: this.get('review.qowp'),
+        prob: this.get('review.prob'),
+        imef: this.get('review.imef'),
+        opfb: this.get('review.opfb'),
+        team: this.get('review.team'),
+        comm: this.get('review.comm'),
+        efco: this.get('review.efco'),
+        reli: this.get('review.reli'),
+        mgmt: this.get('review.mgmt')
+      }
+      this.sendAction('updateReview', review)
+    }
+  }
 });
--- app/routes/review.js
+++ app/routes/review.js
@@ -3,5 +3,22 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model (params) {
     return this.get('store').findRecord('review', params.review_id);
+  },
+  actions: {
+    updateReview (review) {
+      let updatedReview = this.get('store').findRecord('review', review.id)
+        .then(function (update) {
+          update.set('qowp', review.qowp);
+          update.set('prob', review.prob);
+          update.set('imef', review.imef);
+          update.set('opfb', review.opfb);
+          update.set('team', review.team);
+          update.set('comm', review.comm);
+          update.set('efco', review.efco);
+          update.set('reli', review.reli);
+          update.set('mgmt', review.mgmt);
+          update.save();
+        });
+    }
   }
 });
