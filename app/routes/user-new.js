5529bccadd3b419dda65e29bf60d1bfbf0687421
mdcollins80 <mdcollins80@gmail.com>
2017-11-04 13:34:01 -0400

    Adds new user form and action
    - Uses `ember generate component user-new-form` to create new user form.
    - Adds actions to the user-new-form to send new user information up to
      the signUp ajax call.
    - Confirms new users are being generated by Admin user.

--- app/routes/user-new.js
+++ app/routes/user-new.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
+
+  actions: {
+    submit (credentials) {
+      console.log('credentials: ', credentials)
+      this.get('auth').signUp(credentials)
+      .then(() => {
+        this.get('flashMessages')
+        .success('Successfully created new user!')
+      })
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem. Please try again.')
+      })
+    }
+  }
 
 });
