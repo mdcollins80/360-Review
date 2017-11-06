54a88d44c3ad6a6ab78326ab979bef91477beb25
mdcollins80 <mdcollins80@gmail.com>
2017-11-05 20:25:59 -0500

    Removes password from user-update
    - Password updating requires more knowledge of password-digest.  At this
      time I will leave the feature for a future version of the app.

--- app/components/user-update.js
+++ app/components/user-update.js
@@ -8,17 +8,17 @@ export default Ember.Component.extend({
       const user = {
         id: this.get('user.id'),
         email: this.get('user.email'),
-        password: this.get('user.password'),
-        passwordConfirmation: this.get('user.passwordConfirmation'),
+        // password: this.get('user.password'),
+        // passwordConfirmation: this.get('user.passwordConfirmation'),
         givenName: this.get('user.givenName'),
         surname: this.get('user.surname'),
         businessUnit: this.get('user.businessUnit')
       };
-      if (this.get('user.password') !== this.get('user.passwordConfirmation')) {
-        this.get('flashMessages').danger('Passwords don\'t match.  Please try again.')
-      } else {
+      // if (this.get('user.password') !== this.get('user.passwordConfirmation')) {
+      //   this.get('flashMessages').danger('Passwords don\'t match.  Please try again.')
+      // } else {
         this.sendAction('updateUser', user);
-      }
+      // }
     }
   }
 });
